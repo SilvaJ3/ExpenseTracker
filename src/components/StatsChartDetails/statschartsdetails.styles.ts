71ad567fd948a35ac38500d324c6216e9c21317c
SilvaJ3 <80819497+SilvaJ3@users.noreply.github.com>
2022-03-16 16:57:48 +0100

    design stats details - commit avant modif chart stats

--- src/components/StatsChartDetails/statschartsdetails.styles.ts
+++ src/components/StatsChartDetails/statschartsdetails.styles.ts
@@ -8,21 +8,29 @@ export const StatsDetailsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   margin-left: 20px;
+  padding: 10px 20px;
+  background-color: #C4C4C4;
+  height: 500px;
+  overflow-y: scroll;
+  box-shadow: 15px 15px 0px 0px #000000A6;
   @media(max-width: 1024px) {
     align-items: center;
+  }
+  ::-webkit-scrollbar {
+    display: none;
   }
 `
 
 export const StatsDetailsList = styled.ul`
   list-style-type: none;
-  border: solid 1px black;
-  border-radius: 5px;
   padding-left: 0;
   min-width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `
 
 export const StatsDetailsListItem = styled.li<ColorItem>`
-  border-bottom: solid 1px black;
   padding: 5px 10px;
   text-align: right;
   background-color: ${props => (props.bgColor)}
